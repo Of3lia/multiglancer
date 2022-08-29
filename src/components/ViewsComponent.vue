6d1bda68dc0d751bf6fa7e7f28b3304e0683edca
TPNYC\ococucci <ococucci@transperfect.com>
2022-08-29 23:57:09 +0200

    adds padding to views

--- src/components/ViewsComponent.vue
+++ src/components/ViewsComponent.vue
@@ -1,19 +1,20 @@
 <template>
-  <div style="margin:13em 0"></div>
-  <div v-for="(view, i) in views" :key="i" :style="`transform:scale(${scale});`" style="display:inline-block">
-    <div :style="`margin:-${view.height * (1 - scale)}px -${view.width * (1 - scale) / 2}px `">
-      <h3>
-        {{ view.name }} {{ view.width }}x{{ view.height }}
-      </h3>
-      <iframe :src="url" :width="view.width" :height="view.height" loading="lazy" frameborder="1"></iframe>
+  <div style="padding: 13em 2em">
+    <div v-for="(view, i) in views" :key="i" :style="`transform:scale(${scale});`" style="display:inline-block">
+      <div :style="`margin:-${view.height * (1 - scale)}px -${view.width * (1 - scale) / 2}px `">
+        <h3>
+          {{  view.name  }} {{  view.width  }}x{{  view.height  }}
+        </h3>
+        <iframe :src="url" :width="view.width" :height="view.height" loading="lazy" frameborder="1"></iframe>
+      </div>
     </div>
-  </div>
-  <!-- <div v-for="(view, i) in views" :key="i">
+    <!-- <div v-for="(view, i) in views" :key="i">
     <div>
       <iframe :style="`transform:scale(${scale}); transform-origin: 0 0`" :src="url" :width="view.width"
         :height="view.height" loading="lazy" frameborder="1"></iframe>
-    </div>
-  </div> -->
+      </div>
+    </div> -->
+  </div>
 </template>
 
 <script setup lang="ts">
