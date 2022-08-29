cfdd24143e29937be9e8499694ef68421dfa6666
TPNYC\ococucci <ococucci@transperfect.com>
2022-08-30 00:42:54 +0200

    deploy

--- docs/assets/app.3796fb19.js
+++
@@ -1,1 +0,0 @@
-import{$ as e}from"./index.2a52d71d.js";const h=e("app",{state:()=>({url:"https://quasar.dev/",views:[{name:"Pxiel 5",width:393,height:851},{name:"iPhone 12 Pro",width:390,height:844},{name:"PC sm",width:1366,height:768},{name:"PC md",width:1920,height:1080},{name:"PC lg",width:2560,height:1440}]}),getters:{},actions:{}});export{h as u};
